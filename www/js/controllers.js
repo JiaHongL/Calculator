239e0a3540bd969d11775b2f6931c00a2f9e93cd
JiaHongL <jiahongl@jiahongLdeMacBook-Pro.local>
2016-03-08 00:01:51 +0800

    jsonp

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -16,8 +16,8 @@ myApp.controller('calculateCtrl', function($scope,igg) {
 
 	$.ajax({
     type: "get",
-    url: "https://spreadsheets.google.com/feeds/list/1Su8cajlsyRYssKkuOiBuDAVrO8h6hIxlr0ucl1ahsZQ/od6/public/values?alt=json-in-script&callback=?",
-    dataType: "json",
+    url: "https://spreadsheets.google.com/feeds/list/1Su8cajlsyRYssKkuOiBuDAVrO8h6hIxlr0ucl1ahsZQ/od6/public/values?alt=json-in-script",
+    dataType: "jsonp",
 	    success: function(json){
 	        $scope.hero_data =[];
 	        _(json.feed.entry).each(function (item) {
@@ -143,8 +143,8 @@ myApp.controller('calculate2Ctrl', function($scope,igg,$timeout,$ionicLoading,$i
 
 	$.ajax({
     type: "get",
-    url: "https://spreadsheets.google.com/feeds/list/1CvJhQMxeu6Jgh3-KHNXiAhGu5MpG45JkQa8sFwwxBTg/od6/public/values?alt=json-in-script&callback=?",
-    dataType: "json",
+    url: "https://spreadsheets.google.com/feeds/list/1CvJhQMxeu6Jgh3-KHNXiAhGu5MpG45JkQa8sFwwxBTg/od6/public/values?alt=json-in-script",
+    dataType: "jsonp",
 	    success: function(json){
 	        $scope.hero_skill_list =[];
 	        _(json.feed.entry).each(function (item) {
